a9e2513ea0c0f360b8109ca76e794684a3805b18
gracekabaghe <gracema2002@gmail.com>
2014-12-22 13:21:39 +0800

    duplicate-app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DuplicateApp::Application.config.secret_key_base = 'c37419522ecdc7963f6e0a91932cff795fe111055c39aedb1a4d1b5fa79cd961cbe1b179e23d4858eab966801024fa7526308ba9a7c2d25f1ee2cb92d9b1da60'
